feb56d909a5553d49690f84bc4e6872a857fecae
OpinDraws <blohin.m.m@gmail.com>
2025-12-24 07:38:15 +0300

    Apply Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,17 @@
 
 // This is a simple hello world program
 
+
+
 int main() {
+
     std::string name;
+
     std::cout << "Enter your name: ";
+
     std::cin >> name;
+
     std::cout << "Hello world from " << name << std::endl;
+    
     return 0;
 }
