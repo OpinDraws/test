5e5f9493974c27e3f93a00ff69b0aa1243cabe60
OpinDraws <blohin.m.m@gmail.com>
2025-12-24 07:30:37 +0300

    Update hello_world to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-using namespace std; // Ужас, но так надо по заданию
+#include <string>
+using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
     return 0;
 }
